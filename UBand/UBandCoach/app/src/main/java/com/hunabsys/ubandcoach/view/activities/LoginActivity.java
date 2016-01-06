d8296f0f9c830758bbf1e75f49ef5594c47f9c68
Silvia Valdez <silvia.valdez.e@hunabsys.com>
2016-01-05 17:37:30 -0700

    Layout adjustments and refactor of components' names.

--- UBand/UBandCoach/app/src/main/java/com/hunabsys/ubandcoach/view/activities/LoginActivity.java
+++ UBand/UBandCoach/app/src/main/java/com/hunabsys/ubandcoach/view/activities/LoginActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.hunabsys.ubandcoach.R;
@@ -14,7 +13,10 @@ import com.hunabsys.ubandcoach.helpers.FontHelper;
 
 public class LoginActivity extends Activity {
 
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
     private Context mContext;
+    private TextView mTxvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +34,12 @@ public class LoginActivity extends Activity {
         mContext = getApplicationContext();
 
         // Underline TextView's text.
-        TextView txvRegister = (TextView) findViewById(R.id.login_txv_register);
-        txvRegister.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        mTxvSignUp = (TextView) findViewById(R.id.login_txv_sign_up);
+        mTxvSignUp.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
     }
 
     private void addListenersToInternalLoginButtons() {
-        FrameLayout txvRegister = (FrameLayout) findViewById(R.id.login_frame_register);
-        txvRegister.setOnClickListener(new View.OnClickListener() {
+        mTxvSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent;
