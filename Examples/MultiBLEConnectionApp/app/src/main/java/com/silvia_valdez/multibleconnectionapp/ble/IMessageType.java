0aef0af14fa18dc06740c17ed42863b7589bf79d
Silvia Valdez <silvia.valdez.e@gmail.com>
2016-07-25 00:13:42 -0600

    MultiBLEConnectionApp: Adding new project with base layout and some services.

---
+++ Examples/MultiBLEConnectionApp/app/src/main/java/com/silvia_valdez/multibleconnectionapp/ble/IMessageType.java
@@ -0,0 +1,15 @@
+package com.silvia_valdez.multibleconnectionapp.ble;
+
+/**
+ * Interface for Message Types.
+ * Created by silvia.valdez@hunabsys.com on 20/07/16.
+ */
+public interface IMessageType {
+
+    int PROGRESS = 201;
+    int DISMISS = 202;
+    int CLEAR = 301;
+    int STOP_CAPTURE = 1;
+    int RESUME_CAPTURE = 2;
+
+}
