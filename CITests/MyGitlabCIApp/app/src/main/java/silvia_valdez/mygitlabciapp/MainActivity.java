a1e7096b0f3179939fc8f496fd590ea6ef150867
Silvia Valdez <silvia.valdez.e@gmail.com>
2016-08-25 13:29:11 -0600

    GitlabCITests: Added dependencies for Instrumentation Tests. Implemented examples for Unit and Instrumentation Tests taken from: https://io2015codelabs.appspot.com/codelabs/android-studio-testing#1

--- CITests/MyGitlabCIApp/app/src/main/java/silvia_valdez/mygitlabciapp/MainActivity.java
+++ CITests/MyGitlabCIApp/app/src/main/java/silvia_valdez/mygitlabciapp/MainActivity.java
@@ -1,7 +1,11 @@
 package silvia_valdez.mygitlabciapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonSayHello = (Button) findViewById(R.id.main_button_say_hello);
+        buttonSayHello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textHello = (TextView) findViewById(R.id.main_text_hello);
+                EditText editName = (EditText) findViewById(R.id.main_edit_name);
+                textHello.setText(String.format("%s, %s!",
+                        getString(R.string.main_hello), editName.getText().toString()));
+            }
+        });
     }
+
 }
