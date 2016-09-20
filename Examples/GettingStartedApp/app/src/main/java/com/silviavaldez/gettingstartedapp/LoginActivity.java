15966f06d512254ab3178cbcfc7268a5f144c04a
Silvia Valdez <silvia.valdez.e@gmail.com>
2016-09-20 16:57:50 -0600

    GettingStartedApp: Extras, SharedPreferences, validations.

--- Examples/GettingStartedApp/app/src/main/java/com/silviavaldez/gettingstartedapp/LoginActivity.java
+++ Examples/GettingStartedApp/app/src/main/java/com/silviavaldez/gettingstartedapp/LoginActivity.java
@@ -2,16 +2,24 @@ package com.silviavaldez.gettingstartedapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public static final String EXTRA_USER = "com.silviavaldez.gettingstartedapp.EXTRA_USER";
+    public static final String EXTRA_PASS = "com.silviavaldez.gettingstartedapp.EXTRA_PASS";
+
     private Context mContext;
+    private EditText mEditUser;
+    private EditText mEditPass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,21 +27,62 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         initVariables();
+        getSession();
     }
 
     private void initVariables() {
         mContext = getApplicationContext();
 
+        mEditUser = (EditText) findViewById(R.id.login_edit_user);
+        mEditPass = (EditText) findViewById(R.id.login_edit_password);
+
         Button buttonGo = (Button) findViewById(R.id.login_button_go);
         buttonGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(mContext, "Clicked button!", Toast.LENGTH_SHORT).show();
-                Snackbar.make(view, "Clicked button!", Snackbar.LENGTH_SHORT).show();
+//                Toast.makeText(mContext, "Showing Toast!", Toast.LENGTH_SHORT).show();
+//                Snackbar.make(view, "Showing SnackBar!", Snackbar.LENGTH_SHORT).show();
 
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                if (validateUserData()) {
+                    String strUser = mEditUser.getText().toString();
+                    String strPass = mEditPass.getText().toString();
+
+                    goToMainActivity(strUser, strPass);
+
+                } else {
+                    Snackbar.make(view, "Should fill both fields", Snackbar.LENGTH_SHORT).show();
+                }
             }
         });
     }
+
+    private void goToMainActivity(String user, String password) {
+        // Put extras into the intent
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+        Bundle extras = new Bundle();
+        extras.putString(EXTRA_USER, user);
+        extras.putString(EXTRA_PASS, password);
+        intent.putExtras(extras);
+
+        startActivity(intent);
+        finish();
+    }
+
+    private boolean validateUserData() {
+        return !(mEditUser.getText().toString().isEmpty() || mEditPass.getText().toString().isEmpty());
+    }
+
+    private void getSession() {
+        SharedPreferences preferences = getSharedPreferences(MainActivity.PREF_USER_DATA, MODE_PRIVATE);
+        boolean isLoggedIn = preferences.getBoolean(MainActivity.PREF_IS_LOGGED_IN, false);
+
+        if (isLoggedIn) {
+            String name = preferences.getString(MainActivity.PREF_USER_NAME, "No name defined");
+            String pass = preferences.getString(MainActivity.PREF_USER_PASS, "No password defined");
+            Toast.makeText(mContext, String.format("Hello %s!", name), Toast.LENGTH_SHORT).show();
+            goToMainActivity(name, pass);
+        }
+    }
+
 }
--- Examples/GettingStartedApp/app/src/main/java/com/silviavaldez/gettingstartedapp/MainActivity.java
+++ Examples/GettingStartedApp/app/src/main/java/com/silviavaldez/gettingstartedapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.silviavaldez.gettingstartedapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,9 +14,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    public static final String PREF_USER_DATA = "PREF_USER_DATA";
+    public static final String PREF_USER_NAME = "PREF_USER_NAME";
+    public static final String PREF_USER_PASS = "PREF_USER_PASS";
+    public static final String PREF_IS_LOGGED_IN = "PREF_IS_LOGGED_IN";
+
+    private Context mContext;
+    private ListView mListUsers;
+    private ArrayList<String> mArrayUsers;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +45,11 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                Intent settings = new Intent(Settings.ACTION_SETTINGS);
+                startActivity(settings);
             }
         });
 
@@ -40,6 +61,45 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        // Custom methods
+        initVariables();
+        showExtras();
+    }
+
+    private void initVariables() {
+        mContext = getApplicationContext();
+        mListUsers = (ListView) findViewById(R.id.main_list_users);
+        mArrayUsers = new ArrayList<>();
+    }
+
+    private void showExtras() {
+        Bundle extras = this.getIntent().getExtras();
+        String user = extras.getString(LoginActivity.EXTRA_USER);
+        String pass = extras.getString(LoginActivity.EXTRA_PASS);
+
+        saveSession(user, pass);
+
+        mArrayUsers.add(user);
+        mArrayUsers.add("Karen");
+        mArrayUsers.add("Mano");
+        mArrayUsers.add("Jorge");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(mContext,
+                R.layout.item_list_view,
+                mArrayUsers);
+        mListUsers.setAdapter(adapter);
+
+        Toast.makeText(mContext, user + " - " + pass, Toast.LENGTH_SHORT).show();
+    }
+
+    private void saveSession(String name, String pass) {
+        SharedPreferences.Editor editor = getSharedPreferences(PREF_USER_DATA, MODE_PRIVATE).edit();
+        editor.putString(PREF_USER_NAME, name);
+        editor.putString(PREF_USER_PASS, pass);
+        editor.putBoolean(PREF_IS_LOGGED_IN, true);
+        editor.apply();
     }
 
     @Override
