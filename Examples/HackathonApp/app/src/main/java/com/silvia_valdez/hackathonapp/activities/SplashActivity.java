75e7f3b736cde68333684319e124e6938b1ffffe
Silvia Valdez <silvia.valdez.e@gmail.com>
2017-06-01 23:15:22 -0600

    HackathonApp: Added IntroActivity and some adjustments to existing activities.

--- Examples/HackathonApp/app/src/main/java/com/silvia_valdez/hackathonapp/activities/SplashActivity.java
+++ Examples/HackathonApp/app/src/main/java/com/silvia_valdez/hackathonapp/activities/SplashActivity.java
@@ -1,21 +1,18 @@
 package com.silvia_valdez.hackathonapp.activities;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 
 import com.silvia_valdez.hackathonapp.R;
-import com.silvia_valdez.hackathonapp.helpers.UtilHelper;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     private static final int DELAY_TIME = 2000;
 
     private Context context;
-    private ActionBar actionBar;
 
 
     @Override
@@ -24,21 +21,18 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         initVariables();
-        String activityTitle = getString(R.string.app_name);
-        UtilHelper.changeActionBarTextColor(context, actionBar, activityTitle);
         waitAMoment();
     }
 
     private void initVariables() {
         context = getApplicationContext();
-        actionBar = getSupportActionBar();
     }
 
     private void waitAMoment() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                Intent intent = new Intent(SplashActivity.this, IntroActivity.class);
                 startActivity(intent);
                 finish();
             }
